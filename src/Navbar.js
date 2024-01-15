410256d3ddfd0aeb7f802c810bfaf96c969f778e
viveroa2291 <viveroa2291@uwec.edu>
2024-01-14 22:30:13 -0600

    Updates coming along great

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
-
+import './navbar.css';
 function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href='/'>Home</Navbar.Brand>
+            <Navbar.Brand className='main-link' href='/'>Adan Vivero</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <Nav.Link href="/story">Story</Nav.Link>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
-                    <Nav.Link href="/project">Project</Nav.Link>
+                    <Nav.Link className='links' href="/about">About</Nav.Link>
+                    <Nav.Link className='links' href="/story">Story</Nav.Link>
+                    <Nav.Link className='links' href="/contact">Contact</Nav.Link>
+                    <Nav.Link className='links' href="/project">Project</Nav.Link>
+                    <Nav.Link className='twitter-link' href="https://twitter.com/adan_vivero">
+                    <i class="cib-twitter"></i>
+                    
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/component/Home.js
+++ src/component/Home.js
@@ -27,27 +27,18 @@ function Home() {
 */
     return(
     <div id="mainBody">
-        <div>
-            <img class="chicago-image" src={chicago}></img>
+        <div class="top-bar">
+            <span class='picture'>
+                <img class="chicago-image" src={chicago}></img>
+            </span>
+            <div class="introduction">
+                <h2 class="helloWorld">Hi, I'm Adan.</h2>
+                <p>Welcome! Lately you'll find me here scribbling down notes on what I'm learning. If you read anything <br></br> here and have feedback, corrections or thoughts, I'd love to hear from you. </p>
+                <a href="/About">About Me</a>
+            </div>
         </div>
-        <div id="leftBar">
-            <h2 id="helloWorld">Guide</h2>
-            <hr></hr>
-            <p>This website is for you guys to get to know a little bit about me and where I come from and what my interests are. </p>
-            <p>Down below on the footer are a bunch of links that include my social media and links to see my work in regards to computer science. You can also find more in depth on the top navigation bar under <a href="/contact">'Contact'</a>.</p>
-            <p>On the top navigation bar, you can find the <a href="/about">About</a>, <a href="/story">Story, </a> <a href="/contact">Contact</a>, and <a href="/project">Projects</a> links.</p>
-            
-            
+        <hr></hr>
         </div>
-        <div id="description">
-        <div id="previous-button" /* onClick={() => advance(-1)} */> &#8249;</div>
-            <h2 id="header">This is the header</h2>
-            <div id="next-button" /* onClick={() => advance(1)} */ >&#8250;</div>
-            <p>This is the description</p>
-        </div>
-        
-        </div>
-        
     );
 }
 export default Home;

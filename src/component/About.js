87e30bb736cb6e68fd25a9d9342d5ea568d8b0db
viveroa2291 <viveroa2291@uwec.edu>
2024-01-17 21:12:25 -0600

    Changes happening in the story and about me sections of the javascript

--- src/component/About.js
+++ src/component/About.js
@@ -1,73 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./About.css"
-import EarlyLife from './EarlyLife'
-import HighSchool from './HighSchool'
-import College from './College'
-import Now from './Now'
-import Future from './Future'
-import {Row} from 'react-bootstrap'
-import { Button, Collapse } from 'react-bootstrap'
 
 function About() {
-    
-    const [openEarlyLife, showEarlyLIfe] = useState(false);
-    const [openHighSchool, showHighSchool] = useState(false);
-    const [openCollege, showCollege] = useState(false);
-    const [openNow, showNow] = useState(false);
-    const [openFuture, showFuture] = useState(false);
     return (
-        <Row>
-        <div className="biography">
-            <nav className="timeline"> 
-
-            <Button className="early" onClick={() => showEarlyLIfe(!openEarlyLife)} 
-            aria-controls="example-collapse-text"
-            aria-expanded={openEarlyLife}>Early Life</Button>
-
-            <Button className="high" onClick={() => showHighSchool(!openHighSchool)} 
-            aria-controls="example-collapse-text"
-            aria-expanded={openHighSchool}>High School</Button> 
-
-            <Button className="college" onClick={() => showCollege(!openCollege)} 
-            aria-controls="example-collapse-text"
-            aria-expanded={openCollege}>College</Button> 
-
-            <Button className="now" onClick={() => showNow(!openNow)} 
-            aria-controls="example-collapse-text"
-            aria-expanded={openNow}>Now</Button> 
-
-            <Button className="future" onClick={() => showFuture(!openFuture)} 
-            aria-controls="example-collapse-text"
-            aria-expanded={openFuture}>Future</Button>
-                
-            </nav>
-        </div>
-            <Collapse in={openEarlyLife}>
-                <div>
-                    <EarlyLife/>
-                </div>
-            </Collapse>
-            <Collapse in={openHighSchool}>
-                <div>
-                    <HighSchool/>
-                </div>
-            </Collapse>
-            <Collapse in={openCollege}>
-                <div>
-                    <College/>
-                </div>
-            </Collapse>
-            <Collapse in={openNow}>
-                <div>
-                    <Now/>
-                </div>
-            </Collapse>
-            <Collapse in={openFuture}>
-                <div>
-                    <Future/>
-                </div>
-            </Collapse>
-          </Row>
+      <div>
+        <h1>About</h1>
+        <br></br>
+        <p>Hey! I'm Adan. Thanks for visiting my blog.</p>
+        <h2>Me in 20 seconds</h2>
+      </div>
     );
 }
 export default About;
--- src/component/Story.js
+++ src/component/Story.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, {useState}  from 'react'
 
+import EarlyLife from './EarlyLife'
+import HighSchool from './HighSchool'
+import College from './College'
+import Now from './Now'
+import Future from './Future'
+import {Row} from 'react-bootstrap'
+import { Button, Collapse } from 'react-bootstrap'
 function Story (){
+        const [openEarlyLife, showEarlyLIfe] = useState(false);
+        const [openHighSchool, showHighSchool] = useState(false);
+        const [openCollege, showCollege] = useState(false);
+        const [openNow, showNow] = useState(false);
+        const [openFuture, showFuture] = useState(false);
     return (
-        <div>
-            This is my story
+        <Row>
+        <div className="biography">
+            <nav className="timeline"> 
+
+            <Button className="early" onClick={() => showEarlyLIfe(!openEarlyLife)} 
+            aria-controls="example-collapse-text"
+            aria-expanded={openEarlyLife}>Early Life</Button>
+
+            <Button className="high" onClick={() => showHighSchool(!openHighSchool)} 
+            aria-controls="example-collapse-text"
+            aria-expanded={openHighSchool}>High School</Button> 
+
+            <Button className="college" onClick={() => showCollege(!openCollege)} 
+            aria-controls="example-collapse-text"
+            aria-expanded={openCollege}>College</Button> 
+
+            <Button className="now" onClick={() => showNow(!openNow)} 
+            aria-controls="example-collapse-text"
+            aria-expanded={openNow}>Now</Button> 
+
+            <Button className="future" onClick={() => showFuture(!openFuture)} 
+            aria-controls="example-collapse-text"
+            aria-expanded={openFuture}>Future</Button>
+                
+            </nav>
         </div>
-    )
+            <Collapse in={openEarlyLife}>
+                <div>
+                    <EarlyLife/>
+                </div>
+            </Collapse>
+            <Collapse in={openHighSchool}>
+                <div>
+                    <HighSchool/>
+                </div>
+            </Collapse>
+            <Collapse in={openCollege}>
+                <div>
+                    <College/>
+                </div>
+            </Collapse>
+            <Collapse in={openNow}>
+                <div>
+                    <Now/>
+                </div>
+            </Collapse>
+            <Collapse in={openFuture}>
+                <div>
+                    <Future/>
+                </div>
+            </Collapse>
+          </Row>
+    );
 }
 export default Story;

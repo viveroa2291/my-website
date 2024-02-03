c1f24faae929b39d3e75c596d126e3b42aea5794
viveroa2291 <viveroa2291@uwec.edu>
2024-02-02 21:49:05 -0600

    Added more stories

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import './CSS/Home.css';
 import chicago from '../images/chicago.jpeg';
 import coke from '../images/coke.jpg';
+import oop from '../thumbnails/oop.png';
+import threads from '../thumbnails/threads.png';
 function Home() {
 /*
                 <p>Coke has been a very popular drink around the world for a very long time, and the reason why is because of its advertisement techniques. Its Unique Selling Proposition (USP), “Share A Coke” reaches out to a larger audience, because when you purchase the drink, you can get another one and share it with another person even if they’re not much of a coke drinker. Pepsi, on the other hand is very similar to coke, taste wise, but struggles on sales because it doesn’t reach out to a larger audience as coke does. They both may be alike, but sharing the product out to a different group of people is just a smarter strategy on selling your product. Many different consumors purchase coke even though it may not be the most healthiest drink out there, it’s mostly for the taste.</p>
@@ -29,6 +31,168 @@ function Home() {
                 <h2>Share A Coke Draws People In</h2>
                 <p>Many advertisements have a Unique Selling Proposition, and Coke’s is “Share A Coke”. Coke has been dominating its competitors for years but its main competitor “Pepsi”. With a new and different ad campaign, Coke has been remaining above others in its drinks.</p>
                 <p>Last updated: 22 Oct 2015</p>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails threads' src={threads} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
+            </span>
+        </div>
+        <hr></hr>
+        <div className='next-stories'>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Object Oriented Programming</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p> 
+                <h5>Threads</h5>
+            </span>
+            <span>
+                <img className='thumbnails' src={oop} alt="Object Oriented Programming"/>
+                <p>Java</p>
+                <h5>Exceptions</h5>
             </span>
         </div>
         <hr></hr>

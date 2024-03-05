286193ad4c668d2b2dda2170b758516868bf8bc6
viveroa2291 <viveroa2291@uwec.edu>
2024-03-04 18:23:35 -0600

    Code Cleanup

--- src/component/About.js
+++ src/component/About.js
@@ -16,7 +16,7 @@ function About() {
       <div className='about-section'>
           <h1>About</h1>
           <br/>
-          <p>Hey! I'm Adan. Thanks for visiting my blog.</p>
+          <p className='introduction'>Hey! I'm Adan. Thanks for visiting my blog.</p>
           <h2>Me in 20 seconds</h2>
           <p>I'm a full stack developer having worked with all sorts of programming languages. My most proficient programming languages would have to be C++, Java, HTML, CSS, Javascript, and React.js. I have also worked with C, Python, Kotlin, and PHP.</p>
           <p>I have experience working with databases, cloud computing, git, and Api's. The technologies that I use for those are Springboot, AWS, and SQL.</p>
@@ -31,14 +31,17 @@ function About() {
             <TechStack/>
           </Collapse>
           <h2 className='film-header'>My most watched films</h2>
+          <hr/>
           <div className='film-div'>
-            <span className='film'>
-              <iframe className='youtube-videos' width="350" height="157.5" src="https://www.youtube.com/embed/5dZ_lvDgevk?si=EfmImR9pl5V51jwk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
+            <span className='film'>              
               <a className='film-link' href='https://www.youtube.com/watch?v=5dZ_lvDgevk'>In the age of AI</a>  
+              <hr/>
+              <iframe className='youtube-videos' width="350" height="157.5" src="https://www.youtube.com/embed/5dZ_lvDgevk?si=EfmImR9pl5V51jwk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
             </span>
-            <span className='film'>
-              <iframe className='youtube-videos' width="350" height="157.5" src="https://www.youtube.com/embed/1cKE12LK4Eo?si=l8GwtK1N0Wipy5Fd" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
+            <span className='film'>              
               <a className='film-link' href='https://www.youtube.com/watch?v=1cKE12LK4Eo&t=1255s'>AI Tipping Point</a>
+              <hr/>
+              <iframe className='youtube-videos' width="350" height="157.5" src="https://www.youtube.com/embed/1cKE12LK4Eo?si=l8GwtK1N0Wipy5Fd" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
             </span>
           </div>
           <Collapse in={Lense}>

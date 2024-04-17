c8b14eb76c5ab096e9a708303725d0b99e7fe01a
viveroa2291 <viveroa2291@uwec.edu>
2024-04-16 20:56:34 -0500

    Added languages for my projects page

--- src/component/Project.js
+++ src/component/Project.js
@@ -3,22 +3,43 @@ import './CSS/Project.css';
 function Project() {
     return (
         <div  className="project-body">
-           <h1 className='project-header'>These are some of my personal projects</h1>
+           <h1 className='project-header'>Here are some of my personal projects</h1>
            <div className='project-div'>
                 <span className='project-span'>
                     <a className='project-h2' href='https://viveroa2291.github.io/Tic-Tac-Toe/'>Tic Tac Toe</a>
                     <hr className='project-hr'/>
                     <iframe title='tic tac toe game' className='projects' src="https://viveroa2291.github.io/Tic-Tac-Toe/"/>
+                    <p>Languages Used:</p>
+                    <hr/>
+                    <ul className='languages-used'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
                 </span>
                 <span className='project-span'>
                     <a className='project-h2' href='https://viveroa2291.github.io/Cancun/'>Cancun Mexican Grill</a>
                     <hr className='project-hr'/>
                     <iframe title='Cancun Mexican Grill Restaraunt' className='projects' src="https://viveroa2291.github.io/Cancun/"/>
+                    <p>Languages Used:</p>
+                    <hr/>
+                    <ul className='languages-used'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
                 </span>
                 <span className='project-span'>
                     <a className='project-h2' href='https://viveroa2291.github.io/Developer-Site/'>Developer Site</a>
                     <hr className='project-hr'/>
                     <iframe title='My Developer Website' className='projects' src="https://viveroa2291.github.io/Developer-Site/"/>
+                    <p>Languages Used:</p>
+                    <hr/>
+                    <ul className='languages-used'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
                 </span>
            </div>
         </div>

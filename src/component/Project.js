519cdf70997a82ce06c338ef8c8bdd56cda9626c
viveroa2291 <viveroa2291@uwec.edu>
2024-05-11 17:54:05 -0500

    Minor changes

--- src/component/Project.js
+++ src/component/Project.js
@@ -10,7 +10,7 @@ function Project() {
                     <a className='project-h2' href='https://viveroa2291.github.io/Tic-Tac-Toe/' title='My tic tac toe app'>Tic Tac Toe</a>
                     <hr className='project-hr'/>
                     <iframe title='tic tac toe game' className='projects' src="https://viveroa2291.github.io/Tic-Tac-Toe/"/>
-                    <p>Languages Used:</p>
+                    <p>Technologies Used:</p>
                     <hr/>
                     <img className="tech-stack" src='https://img.shields.io/badge/HTML-E34F26?style=for-the-badge&logo=html5&logoColor=white' alt='HTML Icon'/>
                     <img className="tech-stack" src='https://img.shields.io/badge/CSS-1572B6?style=for-the-badge&logo=css3&logoColor=white' alt='CSS Icon'/>                    
@@ -22,7 +22,7 @@ function Project() {
                     <a className='project-h2' href='https://viveroa2291.github.io/Cancun/' title='Cancun Mexican Grill website itself.'>Cancun Mexican Grill</a>
                     <hr className='project-hr'/>
                     <iframe title='Cancun Mexican Grill Restaraunt' className='projects' src="https://viveroa2291.github.io/Cancun/"/>
-                    <p>Languages Used:</p>
+                    <p>Technologies Used:</p>
                     <hr/>
                     <img className="tech-stack" src='https://img.shields.io/badge/HTML-E34F26?style=for-the-badge&logo=html5&logoColor=white' alt='HTML Icon'/>
                     <img className="tech-stack" src='https://img.shields.io/badge/CSS-1572B6?style=for-the-badge&logo=css3&logoColor=white' alt='CSS Icon'/>                    
@@ -34,7 +34,7 @@ function Project() {
                     <a className='project-h2' href='https://www.youtube.com/watch?v=tDRNQQE5oJo' title='A live demo of the app running.'>Mappins</a>
                     <hr className='project-hr'/>
                     <img className='mappins-image' src={mappins} alt='Mobile Application'/>
-                    <p>Languages Used:</p>
+                    <p>Technologies Used:</p>
                     <hr/>
                     <img className="tech-stack" src='https://img.shields.io/badge/Kotlin-0095D5?style=for-the-badge&logo=kotlin&logoColor=white' alt='Kotlin Icon'/>
                     <img className="tech-stack" src='https://img.shields.io/badge/Firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=fff' alt='CSS Icon'/>                    
@@ -45,7 +45,7 @@ function Project() {
                     <a className='project-h2' title='The developer website' href='https://viveroa2291.github.io/Developer-Site/'>Developer Site</a>
                     <hr className='project-hr'/>
                     <iframe title='My Developer Website' className='projects' src="https://viveroa2291.github.io/Developer-Site/"/>
-                    <p>Languages Used:</p>
+                    <p>Technologies Used:</p>
                     <hr/>
                     <img className="tech-stack" src='https://img.shields.io/badge/HTML-E34F26?style=for-the-badge&logo=html5&logoColor=white' alt='HTML Icon'/>
                     <img className="tech-stack" src='https://img.shields.io/badge/CSS-1572B6?style=for-the-badge&logo=css3&logoColor=white' alt='CSS Icon'/>                    

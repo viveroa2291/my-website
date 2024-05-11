a528610e175f28fc4f1a6d33cbf712063b754967
viveroa2291 <viveroa2291@uwec.edu>
2024-05-11 17:48:04 -0500

    Added a project under the projects section

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './CSS/Project.css';
+import mappins from '../images/mappins.jpeg';
 function Project() {
     return (
         <div  className="project-body">
            <h1 className='project-header'>Here are some of my personal projects</h1>
            <div className='project-div'>
                 <span className='project-span'>
-                    <a className='project-h2' href='https://viveroa2291.github.io/Tic-Tac-Toe/'>Tic Tac Toe</a>
+                    <a className='project-h2' href='https://viveroa2291.github.io/Tic-Tac-Toe/' title='My tic tac toe app'>Tic Tac Toe</a>
                     <hr className='project-hr'/>
                     <iframe title='tic tac toe game' className='projects' src="https://viveroa2291.github.io/Tic-Tac-Toe/"/>
                     <p>Languages Used:</p>
@@ -18,7 +19,7 @@ function Project() {
                     <img className="tech-stack" src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git-Icon"/>
                 </span>
                 <span className='project-span'>
-                    <a className='project-h2' href='https://viveroa2291.github.io/Cancun/'>Cancun Mexican Grill</a>
+                    <a className='project-h2' href='https://viveroa2291.github.io/Cancun/' title='Cancun Mexican Grill website itself.'>Cancun Mexican Grill</a>
                     <hr className='project-hr'/>
                     <iframe title='Cancun Mexican Grill Restaraunt' className='projects' src="https://viveroa2291.github.io/Cancun/"/>
                     <p>Languages Used:</p>
@@ -30,7 +31,18 @@ function Project() {
                     <img className="tech-stack" src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git-Icon"/>
                 </span>
                 <span className='project-span'>
-                    <a className='project-h2' href='https://viveroa2291.github.io/Developer-Site/'>Developer Site</a>
+                    <a className='project-h2' href='https://www.youtube.com/watch?v=tDRNQQE5oJo' title='A live demo of the app running.'>Mappins</a>
+                    <hr className='project-hr'/>
+                    <img className='mappins-image' src={mappins} alt='Mobile Application'/>
+                    <p>Languages Used:</p>
+                    <hr/>
+                    <img className="tech-stack" src='https://img.shields.io/badge/Kotlin-0095D5?style=for-the-badge&logo=kotlin&logoColor=white' alt='Kotlin Icon'/>
+                    <img className="tech-stack" src='https://img.shields.io/badge/Firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=fff' alt='CSS Icon'/>                    
+                    <br/>
+                    <img className="tech-stack" src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git-Icon"/>
+                </span> 
+                <span className='project-span'>
+                    <a className='project-h2' title='The developer website' href='https://viveroa2291.github.io/Developer-Site/'>Developer Site</a>
                     <hr className='project-hr'/>
                     <iframe title='My Developer Website' className='projects' src="https://viveroa2291.github.io/Developer-Site/"/>
                     <p>Languages Used:</p>
